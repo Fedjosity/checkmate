e985c199f1f3665e10acc212db0174a88839dad0
Fedjost Ayomide <fedjostayomide@gmail.com>
2026-07-03 09:06:21 +0100

    added toast to utils

--- apps/web/app/providers.tsx
+++ apps/web/app/providers.tsx
@@ -2,7 +2,7 @@
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useState } from "react";
-import { Toaster } from "@/components/ui/Toaster";
+import { Toaster } from "@/components/utils/Toaster";
 import { AuthProvider } from "@/components/providers/AuthProvider";
 
 export function Providers({ children }: { children: React.ReactNode }) {
